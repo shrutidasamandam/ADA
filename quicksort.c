8d90775db036f6bdfa9850eda9c9d500225aa095
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2022-07-23 16:38:01 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -6,64 +6,72 @@
 #include <stdlib.h>
 #include <time.h>
 
-void exch(int *a, int *b)
+void swap(int *x, int *y)
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
 
-int part(int a[], int left, int right)
+
+
+int partition(int array[],int low, int high)
 {
-    int p,i,j;
-    p = left;
-    i = left;
-    j = right;
-    while (i<=j)
+  int pivot_index = low + (rand()%(high-low));
+  if(pivot_index!=high)
+  swap(&array[pivot_index],&array[high]);
+  int pivot_value = array[high];
+  int i = low;
+  for (int j = low; j < high; j++)
+  {
+    if(array[j] <= pivot_value)
     {
-        while(a[i]<=a[p])
-        i++;
-        while(a[j]>a[p])
-        j--;
-        exch(&a[i],&a[j]);
+      swap(&array[i],&array[j]);
+      i++;
     }
-    exch(&a[i],&a[j]);
-    exch(&a[j],&a[p]);
-    return j;
+  }
+  swap(&array[i],&array[high]);
+  return i;
 }
 
-void quicksort(int a[], int left, int right)
+void quicksort_recur(int array[],int low, int high)
 {
-    int s;
-    if(left<right)
-    {
-        s = part(a,left,right);
-        quicksort(a,left,s-1);
-        quicksort(a,s+1,right);
-    }
+  if(low<high)
+  {
+    int pivot_index = partition(array,low,high);
+    quicksort_recur(array,low,pivot_index-1);
+    quicksort_recur(array,pivot_index+1,high);
+  }
+}
+
+void quicksort(int array[],int length)
+{
+  quicksort_recur(array,0,length-1);
 }
 
 int main()
 {
-    int a[10000000],n,i;
-    clock_t start, stop;
-    printf("\nEnter the size of the array");
-    scanf("%d",&n);
-    printf("\nElements before sorting");
-    for(i=0;i<n;i++)
-    {
-        a[i] = rand()%100;
-        printf("%d\t",a[i]);
-    }
-    start = clock();
-    quicksort(a,0,n);
-    stop = clock();
-    stop = stop-start;
-    printf("\nElements after sorting");
-      for(i=0;i<n;i++)
-    {
-        printf("%d\t",a[i]);
-    }
-    printf("\nTime taken = %ld",stop/CLOCKS_PER_SEC);
+  int k;
+  // create a test array
+  /*
+  printf("\nEnter length of array");
+  scanf("\n%d",&length);
+  printf("\nEnter elements of array: ");
+  for(k=1; k<length; k++)
+  {
+    scanf("%d\t",&a[k]);
+  }
+  */
+  int a[] = {10,11,23,44,8,15,3,9,12,45,56,45,45};
+  int length = 13;
+  // use quicksort to sort the array
+  quicksort(a, length);
+  
+  // print out the array to ensure it has been sorted
+  for (int i = 0; i < length; i++)
+    printf("%d ", a[i]);
+  printf("\n");
+
+  return 0;
 }
