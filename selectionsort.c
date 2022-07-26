bb46068e386440f2535355364b904b0066bc26a8
shrutidasamandam <44689940+shrutidasamandam@users.noreply.github.com>
2022-07-26 16:16:18 +0530

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,44 +1,55 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
-int selectionsort(int a[1000],int length)
+  
+void swap(int *x, int *y)
 {
-  for(int i = 0; i < length - 1; i++)
-    {
-        int min_pos = i;
-        for(int j = i+1; j<length; j++)
-        {
-            if(a[j] < a[min_pos])
-            min_pos = j;
-            
-        if(min_pos!=i)
-        {
-            int temp = a[i];
-            a[i] = a[min_pos];
-            a[min_pos] = temp;
-        }
-        }
-    }
-    for(int i=0; i<length; i++)
-    printf("a[%d]=%d\n",i,a[i]);
-    return 0;
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
-
+  
+void selectionsort(int list[], int size)
+{
+    int i, j, min;
+  
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < size-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min = i;
+        for (j = i+1; j < size; j++)
+          if (list[j] < list[min])
+            min = j;
+  
+        // Swap the found minimum element with the first element
+        swap(&list[min], &list[i]);
+    }
+}
+  
+/* Function to print an array */
+void printArray(int list[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", list[i]);
+    printf("\n");
+}
+  
+// Driver program to test above functions
 int main()
 {
-    int a[10000],length,k;
-    clock_t c;
-    printf("\nEnter length of array: ");
-    scanf("%d",&length);
-    printf("\nEnter elements of array");
-    for(k=0; k<length; k++)
+    int size, list[25];
+
+    printf("Enter the size of the list: ");
+    scanf("%d",&size);
+
+    printf("Enter the elements of the list: ");
+    for(int i=0;i<size;i++)
     {
-      scanf("%d",&a[k]);
+        scanf("%d",&list[i]);
     }
-    c = clock();
-    selectionsort(a,length);
-    c = c - clock();
-    printf("\nTime taken to sort is = %ld",c/CLOCKS_PER_SEC);
+    
+    selectionsort(list, size);
+    printf("After selection sort: \n");
+    printArray(list, size);
     return 0;
 }
